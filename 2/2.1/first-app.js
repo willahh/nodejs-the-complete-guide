4c8eb6e6e80d5abb7a8da7aca3e37dabb1fc5699
William Ravel <wravel@gmail.com>
2024-03-06 15:29:20 +0100

    Chapter 2

---
+++ 2/2.1/first-app.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+
+fs.writeFileSync("hello.tsx", "Hello from NodeJS again");
+
+
---
+++ 2/2.12/play.js
@@ -0,0 +1,14 @@
+const fetchData = callback => {
+    setTimeout(() => {
+        callback('Done !');
+    }, 2000);
+};
+
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData((text) => {
+        console.log(text)
+    });
+}, 2000); 
+
+console.log('Hello');
